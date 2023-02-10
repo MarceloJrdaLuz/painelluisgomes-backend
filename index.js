21c91fa01160f7e2479e7bfbb3b4c164d30d726c
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2023-02-10 14:38:56 -0300

    remove app use

--- index.js
+++ index.js
@@ -28,37 +28,4 @@ app.use(
 
 app.use(require("./src/routes"));
 
-app.use(function(req, res, next) {
-  var allowedOrigins = [
-    "ttps://luisgomesrn.vercel.app/"
-  ];
-  var origin = req.headers.origin;
-  console.log(origin)
-  console.log(allowedOrigins.indexOf(origin) > -1)
-  // Website you wish to allow to
-  if (allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-  
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,Authorization"
-  );
-  
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  
-  // Pass to next layer of middleware
-  next();})
-
 app.listen(process.env.PORT || 3000);
